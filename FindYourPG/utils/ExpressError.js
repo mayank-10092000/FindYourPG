910ef238c30980aab85f770e4c6c614d70b1483c
Mayank <153618748+mayank-10092000@users.noreply.github.com>
2024-07-25 17:57:36 +0530

    Delete FindYourPG directory

--- FindYourPG/utils/ExpressError.js
+++
@@ -1,9 +0,0 @@
-class ExpressError extends Error {
-  constructor(message, statusCode) {
-    super();
-    this.message = message;
-    this.statusecode = statusCode;
-  }
-}
-
-module.exports = ExpressError;
